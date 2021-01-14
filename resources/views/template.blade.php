c775a369179ba32b326ccf7b55b26578a8546442
Marcio <marcio20peres@gmail.com>
2021-01-14 18:39:34 -0300

    teste de alteração no css de index

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -7,11 +7,9 @@
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
     <script src="{{asset('js/jquery.js')}}"></script>
     <script src="{{asset('js/bootstrap.js')}}"></script>
-   
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    
-    <title>SENAI</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    <title>SIGMA</title>
 </head>
 <body class="d-flex flex-column">
     <nav class="navbar navbar-dark bg-dark d-flex">
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function() {
-    return view('login');
+    return view('partials.index');
 })->name('index');
 
 Route::get('/home', function() {
