bacd51a00d17eabfdab0d1ca31195f6ed5d5d7be
Jonas <jonasvieirafelix@gmail.com>
2021-01-25 13:53:36 -0300

    jqury

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,9 +6,20 @@ function letsDrop(ev){
     ev.preventDefault();
 }
 
+function errorMsg(error){
+    if(error == 0){
+        $("#msg-btn-close").show(227);
+        $("#lista-error").text('/!\\  • O docente xxxxx já está alocado na segunda no hórario hh:hh até as hh:hh • /!\\ ');   
+    } else {
+        $("#msg-btn-close").hide(227); 
+    }
+};
+
+
+
 function drop(ev, el){
-    
-    if(ev.target.children.length < 1){
+   
+    if(ev.target.children.length < 1 && !error(ev.dataTransfer.getData('srcID'))){
         ev.preventDefault();
         var data = ev.dataTransfer.getData('srcID');
         var nodeCopy = document.getElementById(data).cloneNode(true);
@@ -26,6 +37,17 @@ function drop(ev, el){
             $(el).children('.dropup').children('.drop-ctn').children('.icons').children('img.eqp').removeClass('opacity-20');
             $(el).children('.dropup').children('.dropdown-menu').children('.linha').children('.recurso.eqp').children('p').text(nodeCopy.innerHTML);
         }
+    }
+}
+
+function error(id){
+
+    if(id == 'doc-1'){
+        errorMsg(0);
+        return true;
+    } else{
+        
+        return false;
     }
 }
 
--- resources/views/partials/turma.blade.php
+++ resources/views/partials/turma.blade.php
@@ -6,30 +6,37 @@
 @endsection
 
 @section('content')
-
-    <div class="pg-ctn h-75 bg-light d-flex flex-column">
+    <div class="pg-ctn h-75 bg-light flex-column">
+        <div class="bg-warning msg-error rounded justify-content-around" id = 'msg-btn-close' style="display: none">
+             <div type="button" class="close mr-2" aria-label="Close" onclick="errorMsg(1);">
+                <span aria-hidden="false">&times;</span>
+            </div>
+            <div id="lista-error" class="px-2"></div>
+        </div>
+        
         <div class="config-ctn w-100 h-75 d-flex flex-lg-column align-items-center justify-content-around">
             @yield('cai/tec')
             <form class="w-25 d-flex justify-content-around m-2">
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                        <label class="form-check-label" for="exampleRadios1">
-                            Manhã
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                        <label class="form-check-label" for="exampleRadios2">
-                            Tarde
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
-                        <label class="form-check-label" for="exampleRadios2">
-                            Noite
-                        </label>
-                    </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                    <label class="form-check-label" for="exampleRadios1">
+                        Manhã
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                    <label class="form-check-label" for="exampleRadios2">
+                        Tarde
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+                    <label class="form-check-label" for="exampleRadios2">
+                        Noite
+                    </label>
+                </div>
             </form>
+            
             <div class="days h-65 w-100 d-flex justify-content-around">
                 @for ($i = 0; $i < 5; $i++)
                     @switch($i)
@@ -70,7 +77,7 @@
             <div class="side h-100 w-18 d-flex flex-column align-items-center justify-content-center">
                 <h4 class="text-center">Docentes</h4>
                     <div class="w-75 px-2 h-75 d-flex flex-column align-items-center overflow-overlay">
-                        <div class="docente w-65 side-item bg-white text-center" draggable="true" id='doc-1' ondragstart="drag(event);">Docente 1</div>
+                        <div class="docente w-65 side-item bg-white text-center" draggable="true" id='doc-1' ondragstart="drag(event);">Docente 1 alocado</div>
                         <div class="docente w-65 side-item bg-white text-center" draggable="true" id='doc-2' ondragstart="drag(event);" data-toggle="tooltip" data-placement="bottom">Docente 2</div>
                         <div class="docente w-65 side-item bg-white text-center" draggable="true" id='doc-3' ondragstart="drag(event);" data-toggle="tooltip" data-placement="bottom">Docente 3</div>
                         <div class="docente w-65 side-item bg-white text-center" draggable="true" id='doc-4' ondragstart="drag(event);">Docente 4</div>
